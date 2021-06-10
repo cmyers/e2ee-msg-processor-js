5603182ee9dd50f36b7ff1eb75778913344ff2e1
Chris Myers <cmyers2015@outlook.com>
2021-06-11 00:56:02 +0100

    Update webpack config and package.json for prod and dev modes

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  mode: 'development',
   target: 'node',
   devtool: "source-map",
   externals: [nodeExternals()],
