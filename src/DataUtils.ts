284254f17c7fb63612f3249f3ef2d74d53b950d2
Chris Myers <cmyers2015@outlook.com>
2021-06-22 23:54:57 +0100

    DataUtils refactor

--- src/DataUtils.ts
+++ src/DataUtils.ts
@@ -5,20 +5,15 @@ export class DataUtils {
     }
   
     static base64StringToArrayBuffer(str: string): ArrayBuffer {
-      return this.bufferToArrayBuffer(Buffer.from(str, 'base64'));
+      return new Uint8Array(Buffer.from(str, 'base64')).buffer;
     }
-  
-    static bufferToArrayBuffer(buffer: Buffer): ArrayBuffer {
-      const ab = new ArrayBuffer(buffer.length);
-      const view = new Uint8Array(ab);
-      for (let i = 0; i < buffer.length; ++i) {
-        view[i] = buffer[i];
-      }
-      return ab;
+
+    static bufferToBase64String(buffer: Buffer): string {
+      return this.arrayBufferToBase64String(buffer);
     }
   
     static stringToArrayBuffer(str: string): ArrayBuffer {
-      return this.bufferToArrayBuffer(Buffer.from(str));
+      return new Uint8Array(Buffer.from(str)).buffer;
     }
   
     static arrayBufferToString(arrayBuffer: ArrayBuffer): string {
@@ -26,11 +21,11 @@ export class DataUtils {
     }
   
     static encodeBase64(str: string): string {
-      return Buffer.from(str, 'binary').toString('base64');
+      return Buffer.from(str).toString('base64');
     }
   
     static decodeBase64(str: string): string {
-      return Buffer.from(str, 'base64').toString('binary');
+      return Buffer.from(str, 'base64').toString();
     }
   
     static appendArrayBuffer(buffer1: ArrayBuffer, buffer2: ArrayBuffer): ArrayBuffer {
