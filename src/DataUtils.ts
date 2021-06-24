94d8d8d2f077ebf689d87a7a8e1d2635d6061e43
Chris Myers <cmyers2015@outlook.com>
2021-06-24 22:14:45 +0100

    Consoldate string and buffer encoding. Remove unrequired DataUtils methods. Formatting

--- src/DataUtils.ts
+++ src/DataUtils.ts
@@ -1,41 +1,8 @@
 export class DataUtils {
-
-    static arrayBufferToBase64String(arrayBuffer: ArrayBuffer): string {
-      return Buffer.from(arrayBuffer).toString('base64');
-    }
-  
-    static base64StringToArrayBuffer(str: string): ArrayBuffer {
-      return new Uint8Array(Buffer.from(str, 'base64')).buffer;
-    }
-
-    static bufferToBase64String(buffer: Buffer): string {
-      return this.arrayBufferToBase64String(buffer);
-    }
-    
-    static base64StringToBuffer(str: string): Buffer {
-      return Buffer.from(str, 'base64');
-    }
-  
-    static stringToArrayBuffer(str: string): ArrayBuffer {
-      return new Uint8Array(Buffer.from(str)).buffer;
-    }
-  
-    static arrayBufferToString(arrayBuffer: ArrayBuffer): string {
-      return Buffer.from(arrayBuffer).toString();
-    }
-  
-    static encodeBase64(str: string): string {
-      return Buffer.from(str).toString('base64');
-    }
-  
-    static decodeBase64(str: string): string {
-      return Buffer.from(str, 'base64').toString();
-    }
-  
-    static appendArrayBuffer(buffer1: ArrayBuffer, buffer2: ArrayBuffer): ArrayBuffer {
-      const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-      tmp.set(new Uint8Array(buffer1), 0);
-      tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
-      return tmp.buffer;
-    }
+  static appendArrayBuffer(buffer1: ArrayBuffer, buffer2: ArrayBuffer): ArrayBuffer {
+    const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+    tmp.set(new Uint8Array(buffer1), 0);
+    tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+    return tmp.buffer;
   }
+}
--- src/MessageProcessor.ts
+++ src/MessageProcessor.ts
@@ -1,5 +1,6 @@
 
 import { Crypto, CryptoKey } from "@peculiar/webcrypto";
+import { TextEncoder } from "util";
 import { DataUtils } from "./DataUtils";
 import { EncryptedMessage } from "./EncryptedMessage";
 import { Key } from "./Key";
@@ -24,9 +25,9 @@ export class MessageProcessor {
 
     private async encryptKey(jid: string, deviceId: number, key: CryptoKey, length: number, encryptedText: ArrayBuffer): Promise<Key> {
         const tag = encryptedText.slice(length),
-        exported_key = await crypto.subtle.exportKey('raw', key),
-        key_tag = DataUtils.arrayBufferToBase64String(DataUtils.appendArrayBuffer(exported_key, tag)),
-        encryptedKey = this._sessionManager.encryptKey(key_tag, jid, deviceId);
+            exported_key = await crypto.subtle.exportKey('raw', key),
+            key_tag = Buffer.from(DataUtils.appendArrayBuffer(exported_key, tag)).toString('base64'),
+            encryptedKey = this._sessionManager.encryptKey(key_tag, jid, deviceId);
 
         return {
             jid,
@@ -38,34 +39,34 @@ export class MessageProcessor {
 
     async encryptMessage(jid: string, plaintext: string): Promise<EncryptedMessage> {
         const iv = crypto.getRandomValues(new Uint8Array(12)),
-        key = await crypto.subtle.generateKey(KEY_ALGO, true, ['encrypt', 'decrypt']),
-        algo = {
-            'name': 'AES-GCM',
-            'iv': iv,
-            'tagLength': TAG_LENGTH
-        },
-        encrypted = await crypto.subtle.encrypt(algo, key, DataUtils.stringToArrayBuffer(plaintext)),
-        length = encrypted.byteLength - 16,
-        ciphertext = encrypted.slice(0, length);
+            key = await crypto.subtle.generateKey(KEY_ALGO, true, ['encrypt', 'decrypt']),
+            algo = {
+                'name': 'AES-GCM',
+                'iv': iv,
+                'tagLength': TAG_LENGTH
+            },
+            encrypted = await crypto.subtle.encrypt(algo, key, new TextEncoder().encode(plaintext)),
+            length = encrypted.byteLength - 16,
+            ciphertext = encrypted.slice(0, length);
 
         const deviceIds = this._sessionManager.deviceIdsFor(jid);
         const keys: Array<Key> = [];
 
-        for(const i in deviceIds) {
+        for (const i in deviceIds) {
             keys.push(await this.encryptKey(jid, deviceIds[i], key, length, encrypted));
         }
 
-        if(jid !== this._sessionManager.JID) {
+        if (jid !== this._sessionManager.JID) {
             const jidDeviceIds = this._sessionManager.deviceIdsFor(this._sessionManager.JID);
 
-            for(const i in jidDeviceIds) {
+            for (const i in jidDeviceIds) {
                 keys.push(await this.encryptKey(this._sessionManager.JID, jidDeviceIds[i], key, length, encrypted));
             }
         }
 
         const sid = this._sessionManager.Store.get(DEVICE_ID);
 
-        if(!sid) {
+        if (!sid) {
             throw new Error("Sender device ID missing from store");
         }
 
@@ -76,28 +77,28 @@ export class MessageProcessor {
             from: this._sessionManager.JID,
             header: {
                 sid: sidParsed,
-                iv_base64: DataUtils.arrayBufferToBase64String(iv),
+                iv_base64: Buffer.from(iv).toString('base64'),
                 keys
             },
-            payload_base64: DataUtils.arrayBufferToBase64String(ciphertext)
+            payload_base64: Buffer.from(ciphertext).toString('base64')
         }
     }
 
     private async decryptMessage(encryptedMessage: EncryptedMessage, decryptedKey: string): Promise<string> {
-        const key = DataUtils.base64StringToArrayBuffer(decryptedKey).slice(0, 16);
-        const tag = DataUtils.base64StringToArrayBuffer(decryptedKey).slice(16);
+        const key = Buffer.from(decryptedKey, 'base64').slice(0, 16);
+        const tag = Buffer.from(decryptedKey, 'base64').slice(16);
         const key_obj = await crypto.subtle.importKey('raw', key, KEY_ALGO, true, ['encrypt', 'decrypt']);
-        const cipher = DataUtils.appendArrayBuffer(DataUtils.base64StringToArrayBuffer(encryptedMessage.payload_base64), tag);
+        const cipher = DataUtils.appendArrayBuffer(Buffer.from(encryptedMessage.payload_base64, 'base64'), tag);
 
         const algo = {
             'name': 'AES-GCM',
-            'iv': DataUtils.base64StringToArrayBuffer(encryptedMessage.header.iv_base64),
+            'iv': Buffer.from(encryptedMessage.header.iv_base64, 'base64'),
             'tagLength': TAG_LENGTH
         };
 
         const decryptedArrayBuffer = await crypto.subtle.decrypt(algo, key_obj, cipher);
 
-        return DataUtils.arrayBufferToString(decryptedArrayBuffer);
+        return Buffer.from(decryptedArrayBuffer).toString();
     }
 
     // TODO handle decryption failure cases
@@ -109,13 +110,13 @@ export class MessageProcessor {
     async processMessage(encryptedMessage: EncryptedMessage): Promise<string | null> {
         const decryptedKey = await this._sessionManager.decryptKey(encryptedMessage);
 
-        if(!decryptedKey) {
+        if (!decryptedKey) {
             return null;
         }
 
         const deviceIds = this._sessionManager.deviceIdsFor(encryptedMessage.from);
 
-        if(!deviceIds.some(x => x === encryptedMessage.header.sid)) {
+        if (!deviceIds.some(x => x === encryptedMessage.header.sid)) {
             this._sessionManager.updateDeviceIds(encryptedMessage.from, [encryptedMessage.header.sid]);
         }
 
