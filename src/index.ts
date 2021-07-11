bd39e16bb11dd9d0d831e7f30e1ed7e366435ed6
Chris Myers <cmyers2015@outlook.com>
2021-07-11 21:58:57 +0100

    Update index exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from "./OmemoManager";
 export * from "./DataUtils";
 export * from "./EncryptedMessage";
 export * from "./LocalStorage";
+export * from "./Bundle";
+export * from "./PreKey";
